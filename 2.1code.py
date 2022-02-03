a1506dcdf6dfae8dad788377a18fd51dcf0594d0
Arki <czoboly.mihaly@gmail.com>
2022-02-03 21:26:32 +0100

    removed setup-related lines

--- 2.1code.py
+++ 2.1code.py
@@ -1,18 +1,8 @@
-import board
-import busio
-from digitalio import DigitalInOut, Direction, Pull
-import time
-import adafruit_max31865
-from adafruit_character_lcd.character_lcd import Character_LCD_Mono
 import math
+import time
 from evaluate_temperature import Temperature
 from control import ControlTemperature
-
-# setup Max_31865 sensor
-spi = busio.SPI(clock=board.GP10, MOSI=board.GP11, MISO=board.GP12)
-cs = DigitalInOut(board.GP13)
-sensor = adafruit_max31865.MAX31865(spi, cs, rtd_nominal=100, ref_resistor=430.0, wires=3)
-temp_actual = sensor.temperature
+from setup import *
 
 # set initial required temperature
 with open("/temp.txt", "r") as t:
@@ -38,37 +28,11 @@ if log_pin.value:
 else:
     log_enabled = False
 
-# set lcd pins
-lcd_rs = DigitalInOut(board.GP16)
-lcd_en = DigitalInOut(board.GP17)
-lcd_d4 = DigitalInOut(board.GP18)
-lcd_d5 = DigitalInOut(board.GP19)
-lcd_d6 = DigitalInOut(board.GP20)
-lcd_d7 = DigitalInOut(board.GP21)
-
-# set lcd size
-lcd_columns = 16
-lcd_rows = 2
-
 # initialize lcd
-lcd = Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
-
 lcd.clear()
 lcd.message = "Temp: {0:0.1f} C\nSet: {1:0.0f}".format(temp_actual, temp_set)
 
-# setup button up
-btn_up = DigitalInOut(board.GP22)
-btn_up.direction = Direction.INPUT
-btn_up.pull = Pull.DOWN
-
-# setup button down
-btn_down = DigitalInOut(board.GP26)
-btn_down.direction = Direction.INPUT
-btn_down.pull = Pull.DOWN
-
-# setup and initialize relay (at start turned off)
-relay = DigitalInOut(board.GP6)
-relay.direction = Direction.OUTPUT
+# initialize relay (at start turned off)
 relay.value = False
 
 def increase_temp(duration, max=150):
@@ -165,4 +129,5 @@ def main():
             t.temp_max_list = []
             t.temp_min_list = []
 
-main()
+if __name__ == "__main__":
+    main()
